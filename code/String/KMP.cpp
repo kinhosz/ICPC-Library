d01f86051b48c793d1fcce9dc5a3b341bcab0532
José Carlos <39632709+kinhosz@users.noreply.github.com>
2023-04-04 15:10:01 -0300

    Update KMP.cpp

--- code/String/KMP.cpp
+++ code/String/KMP.cpp
@@ -19,7 +19,7 @@ public:
     build(S);
   }
 
-  auto getPrefixes(string &T){
+  auto getPI(string &T){ // longest proper prefix
     vector<int> prefix;
     int j = 0;
     for(int i=0;i<T.size();i++){
@@ -28,5 +28,34 @@ public:
       prefix.push_back(j);
     }
     return prefix;
+  }
+
+  auto getLCP(string &T){ // longest common prefix between T & Patt
+    vector<int> lcp = getPI(T);
+    int pe = 1;
+    for(int i=1;i<patt.size();i++){
+      if(patt[i] != patt[i-1]) break;
+      pe++;
+    }
+
+    for(int i=0;i<lcp.size();i++){
+      if(lcp[i] != 0) lcp[i - lcp[i] + 1] = lcp[i];
+      if(lcp[i] != 1) lcp[i] = 0;
+    }
+    int cnt;
+
+    for(int i=0;i<lcp.size();i++){
+      if(T[i] == patt[0]){
+        if(lcp[i] != 0){
+          cnt = pe;
+        }
+        else{
+          cnt--;
+          lcp[i] = cnt; 
+        }
+      }
+      else lcp[i] = 0;
+    }
+    return lcp;
   }
 };
