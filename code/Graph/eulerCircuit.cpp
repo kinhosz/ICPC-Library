ebad3dc23265d96060e2f2012f7145fe3cb61eaf
kinhosz <jcsc@cin.ufpe.br>
2023-07-13 18:20:54 -0300

    fix euler path

--- code/Graph/eulerCircuit.cpp
+++ code/Graph/eulerCircuit.cpp
@@ -3,17 +3,25 @@ typedef pair<int, int> ii;
 template <const bool BIDIRECTIONAL>
 class EulerPath{
 	vector<ii> edges;
+	vector<int> ids;
 	vector<vector<int>> adj;
 	vector<bool> used;
 	vector<int> dg;
+	int root=0;
 
-	int getRoot(){
+	bool check(int u){
+		if(BIDIRECTIONAL && adj[u].size()%2) return true;
+		if(!BIDIRECTIONAL && dg[u] == 1) return true;
+
+		return false;
+	}
+
+	void setRoot(){
+		if(check(this->root)) return;
+
 		for(int i=0;i<adj.size();i++){
-			if(BIDIRECTIONAL && adj[i].size()%2) return i;
-			else if(!BIDIRECTIONAL && dg[i] == 1) return i;
+			if(check(i)) this->root = i;
 		}
-
-		return 0;
 	}
 
 	bool wasUsed(int e){
@@ -21,19 +29,34 @@ class EulerPath{
 		return used[e];
 	}
 
-	void dfs(int u, vector<ii> &path){
+	void dfs(int u, vector<int> &path){
 		while(!adj[u].empty()){
 			int e = adj[u].back();
 			adj[u].pop_back();
 
 			if(this->wasUsed(e)) continue;
 
-			int id = edges[e].first;
 			int v = edges[e].second;
 			used[e] = true;
 
 			dfs(v, path);
-			path.push_back({id, v});
+			path.push_back(e);
+		}
+	}
+
+	void circuit(vector<int> &path, vector<ii> &ans){
+		int from = this->root;
+
+		for(int i=(int)path.size()-1;i>=0;i--){
+			int e = path[i];
+
+			if(edges[e].first != from){
+				ans.clear();
+				return;
+			}
+
+			from = edges[e].second;
+			ans.push_back({ids[e], edges[e].second});
 		}
 	}
 
@@ -46,7 +69,8 @@ public:
 	void add(int u, int v, int id){
 		for(int i=0;i<=BIDIRECTIONAL;i++){
 			adj[u].push_back(edges.size());
-			edges.push_back({id, v});
+			edges.push_back({u, v});
+			ids.push_back(id);
 			used.push_back(false);
 			dg[u]++;
 			dg[v]--;
@@ -55,10 +79,16 @@ public:
 		}
 	}
 
-	void get(vector<ii> &path){
-		int root = this->getRoot();
+	void get(vector<ii> &ans, int start=0){
+		this->root = start;
+		this->setRoot();
 
-		this->dfs(root, path);
-		reverse(path.begin(), path.end());
+		vector<int> path;
+		this->dfs(this->root, path);
+		circuit(path, ans);
 	}
+
+	int getRoot(){ return this->root; }
 };
+
+// returns a vector of <id, to>
