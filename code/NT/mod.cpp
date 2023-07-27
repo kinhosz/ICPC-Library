c3f7fa4fa466b144ff11cf4597d2ec8c735c08b7
kinhosz <jcsc@cin.ufpe.br>
2023-07-27 14:34:13 -0300

    add modint

--- code/NT/mod.cpp
+++ code/NT/mod.cpp
@@ -1,13 +1,13 @@
-template <typename T = long long>
+template <typename T>
 struct Modint{
 	typedef Modint Mi;
-	const T mod = 1e9+7;
+	const T mod = 1e9;
 	T x = 0;
 
 	Modint(){}
-	Modint(T y){ this->normalize(y); }
+	Modint(T y){ x = normalize(y); }
 
-	T normalize(T y){ while(y<0) y += mod; x = y%mod; }
+	T normalize(T y){ while(y<0) y += mod; return y % mod; }
 
 	Mi fexp(Mi b, T e){
 		Mi r = 1;
@@ -19,22 +19,30 @@ struct Modint{
 		return r;
 	}
 
-	Mi inverse(){ return this->fexp(Mi(x), mod-2); }
+	Mi inverse(){ return fexp(Mi(x), mod-2); }
 
-	Mi& operator=(const T &y){ x = Mi(y).x; }
-	Mi& operator=(const Mi &other) { x = other.x; }
-	Mi operator+(const Mi &other) const { return (x + other.x) % mod; }
-	Mi operator-(const Mi &other) const { return (x - other.x + mod) % mod; }
-	Mi operator*(const Mi &other) const { return (x * other.x) % mod; }
-	Mi operator/(Mi other) const { return (x * other.inverse().x) % mod; }
+	Mi& operator=(const T &y){ x = Mi(y).x; return *this; }
+	Mi& operator=(const Mi &other) { x = other.x; return *this; }
+	Mi operator+(const Mi &other) const { return Mi((x + other.x) % mod); }
+	Mi operator-(const Mi &other) const { return Mi((x - other.x + mod) % mod); }
+	Mi operator*(const Mi &other) { return Mi((x * other.x) % mod); }
+	Mi operator/(Mi other) const { return Mi((x * other.inverse().x) % mod); }
 
-	Mi& operator+=(const Mi &other) { Mi val = (*this) + other; x = val.x; }
-	Mi& operator-=(const Mi &other) { Mi val = (*this) - other; x = val.x; }
-	Mi& operator*=(const Mi &other) { Mi val = (*this) * other; x = val.x; }
-	Mi& operator/=(const Mi &other) { Mi val = (*this) / other; x = val.x; }
+	Mi operator++(int32_t){ Mi cop(*this); x = (x + 1) % mod; return cop; }
+	Mi operator--(int32_t){ Mi cop(*this); x = (x -1 + mod) % mod; return cop; }
+
+	Mi operator++(){ x = (x + 1) % mod; return *this; }
+	Mi operator--(){ x = (x - 1 + mod) % mod; return *this; }
+
+	Mi& operator+=(const Mi &other) { Mi val = (*this) + other; x = val.x; return *this; }
+	Mi& operator-=(const Mi &other) { Mi val = (*this) - other; x = val.x; return *this; }
+	Mi& operator*=(const Mi &other) { Mi val = (*this) * other; x = val.x; return *this; }
+	Mi& operator/=(const Mi &other) { Mi val = (*this) / other; x = val.x; return *this; }
 
 	friend ostream& operator<<(ostream& os, const Mi &other){ 
 		os << other.x; 
 		return os; 
 	}
 };
+
+typedef Modint<long long> ModInt;
